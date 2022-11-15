5f67cf3c2a9848e7dc3490a9cbb94eaf0693ca39
Akoruu.Dev <akoruu.dev@gmail.com>
2022-11-15 12:55:09 -0300

    FIX [Routes]: Rename route from home and sign-in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+// tools
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./styles/globalStyle";
+
+// pages
 import Home from "./pages/contents/Home";
 import Login from "./pages/log/Login";
 import Register from "./pages/log/Register";
 import Inputs from "./pages/contents/Inputs"
 import Outputs from "./pages/contents/Outputs"
-import GlobalStyle from "./styles/globalStyle";
 
 export default function App() {
     return (
@@ -12,9 +15,9 @@ export default function App() {
             <GlobalStyle />
             <BrowserRouter>
                 <Routes>
-                    <Route path='/sign-in' element={ <Login /> } />
+                    <Route path='/' element={ <Login /> } />
                     <Route path='/sign-up' element={ <Register /> } />
-                    <Route path='/' element={ <Home /> } />
+                    <Route path='/home' element={ <Home /> } />
                     <Route path='/new-input' element={ <Inputs /> } />
                     <Route path='/new-output' element={ <Outputs /> } />
                 </Routes>
