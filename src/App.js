9c650307592002e2e3025f51fb7a81e4e348969d
Akoruu.Dev <akoruu.dev@gmail.com>
2022-11-15 11:56:04 -0300

    FEAT [Connect]: Create a function for connect pages on App.js main page from App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/contents/Home";
+import Login from "./pages/log/Login";
+import Register from "./pages/log/Register";
+import Inputs from "./pages/contents/Inputs"
+import Outputs from "./pages/contents/Outputs"
+import GlobalStyle from "./styles/globalStyle";
+
 export default function App() {
     return (
-        <>Hello World</>
+        <>
+            <GlobalStyle />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/sign-in' element={ <Login /> } />
+                    <Route path='/sign-up' element={ <Register /> } />
+                    <Route path='/' element={ <Home /> } />
+                    <Route path='/new-input' element={ <Inputs /> } />
+                    <Route path='/new-output' element={ <Outputs /> } />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
